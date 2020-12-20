265b3e07b670305701fa63dbe4bdd5b7653eae13
unknown <1679901743@qq.com>
2020-12-20 23:15:40 +0800

    add files

---
+++ cmake-build-debug/CMakeFiles/TestGit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestGit.dir/main.cpp.obj"
+  "CMakeFiles/TestGit.dir/test.cpp.obj"
+  "TestGit.exe"
+  "TestGit.exe.manifest"
+  "TestGit.lib"
+  "TestGit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
