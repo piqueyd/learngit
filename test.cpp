67192ad244d55e1928cf0d77f0602a35de72d91c
unknown <1679901743@qq.com>
2020-12-20 23:17:19 +0800

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pique on 2020/12/20.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is first line witten by daniel!" << endl;
+    cout << "This is second line witten by daniel!" << endl;
+    cout << "This is third line witten by daniel!" << endl;
+    return 0;
+}
