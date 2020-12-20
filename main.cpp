52c5e33b98601c80a449cbcd9fe3093296a4c4de
unknown <1679901743@qq.com>
2020-12-20 20:29:31 +0800

    third line

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     cout << "This is first line witten by daniel!" << endl;
     cout << "This is second line witten by daniel!" << endl;
+    cout << "This is third line witten by daniel!" << endl;
     return 0;
 }
 
