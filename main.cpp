0bd56b8c0db57e57343d8c9d31cdac82c44abc67
unknown <1679901743@qq.com>
2020-12-27 18:57:07 +0800

    fourth line

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "This is first line witten by daniel!" << endl;
     cout << "This is second line witten by daniel!" << endl;
     cout << "This is third line witten by daniel!" << endl;
+    cout << "This is fourth line witten by daniel!" << endl;
     return 0;
 }
 
